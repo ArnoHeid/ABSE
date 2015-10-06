3654e50f0c0847f980f3ec511b127758231326fc
Arno <arno@iheidelberg.com>
2015-10-06 09:36:04 +0200

    erste Uebung

---
+++ Uebung1/Uebung1/Module1.vb
@@ -0,0 +1,21 @@
+﻿Imports System.Console
+
+Module Module1
+
+    Sub Main()
+
+        Dim name As String
+        Dim matNr As String
+
+        WriteLine("Hallo lieber Benutzer, bitte geben sie Ihren Namen ein.")
+        name = ReadLine()
+        WriteLine("Bitte geben sie nun Ihre Martrikelnummer ein")
+        matNr = ReadLine()
+        WriteLine("Liebe(r) " & name & " deine Matrikelnummer ist: " & matNr)
+        WriteLine("Danke für die Info")
+        ReadLine()
+
+
+    End Sub
+
+End Module
