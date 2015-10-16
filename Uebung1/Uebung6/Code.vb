5521447e1f0c72f9e9c21e070ae6b51cae7b5d98
arnoheid <arno.dario.ingo.heidelberg@students.hs-mainz.de>
2015-10-16 23:43:41 +0200

    neue klase

---
+++ Uebung1/Uebung6/Code.vb
@@ -0,0 +1,5 @@
+﻿Public Class CodeString
+    Implements ICodeString
+
+    Public MustOverride Function Convert() As String Implements ICodeString.Convert
+End Class
---
+++ Uebung1/Uebung6/ICodeString.vb
@@ -0,0 +1,19 @@
+﻿Imports Uebung6
+
+Public Interface ICodeString
+    Function code(value As String) As String
+    Function decode(value As String) As String
+End Interface
+
+
+Public Class Offset
+    Implements ICodeString
+
+    Public Function code(value As String) As String Implements ICodeString.code
+        Throw New NotImplementedException()
+    End Function
+
+    Public Function decode(value As String) As String Implements ICodeString.decode
+        Throw New NotImplementedException()
+    End Function
+End Class
