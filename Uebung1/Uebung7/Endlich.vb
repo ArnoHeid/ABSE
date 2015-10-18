61a5fdf8fe19b53f494896f1c66cca4495ae37d0
ArnoHeid <arno.dario.ingo.heidelberg@students.hs-mainz.de>
2015-10-18 11:46:51 +0200

    u7: erste Checks

--- Uebung1/Uebung7/Endlich.vb
+++ Uebung1/Uebung7/Endlich.vb
@@ -2,4 +2,57 @@
     Private Sub Button_back_Click(sender As Object, e As EventArgs) Handles Button_back.Click
         Me.Close()
     End Sub
+
+    Private Sub Button_start_Click(sender As Object, e As EventArgs) Handles Button_start.Click
+        If Not isAValied() Then
+
+        ElseIf Not isQValied() Then
+
+        ElseIf Not isDeltaValied() Then
+
+
+        End If
+
+
+        For i As Integer = 1 To 2
+
+        Next
+    End Sub
+
+    Function isAValied() As Boolean
+        If Not IsNumeric(TextBox_Faktor_a.Text) Then
+            MsgBox("a muss eine Zahl sein")
+            Return False
+        Else
+            Dim a_int As Integer = Integer.Parse(TextBox_Faktor_a.Text)
+            If (a_int <> 0) Then
+                Return True
+            Else
+                MsgBox("a muss ungleich 0 sein")
+                Return False
+            End If
+        End If
+
+    End Function
+
+    Function isQValied() As Boolean
+        If Not IsNumeric(TextBox_Faktor_a.Text) Then
+            MsgBox("a muss eine Zahl sein")
+            Return False
+        Else
+            Dim a_int As Integer = Integer.Parse(TextBox_Faktor_a.Text)
+            If (a_int > 1) Then
+                Return True
+            Else
+                MsgBox("a muss größer als 1 sein")
+                Return False
+            End If
+        End If
+
+    End Function
+
+    Function isDeltaValied() As Boolean
+
+    End Function
+
 End Class
