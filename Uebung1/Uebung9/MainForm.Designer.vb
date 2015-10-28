ed1bc149ee4362f9a137f6009092ea3a2baac534
arnoheid <arno.dario.ingo.heidelberg@students.hs-mainz.de>
2015-10-28 08:14:46 +0100

    Nachtrag 9

---
+++ Uebung1/Uebung9/MainForm.Designer.vb
@@ -0,0 +1,158 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem_open = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem_Save = New System.Windows.Forms.ToolStripMenuItem()
+        Me.UnterToolStripMenuItem_SaveAs = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem_exit = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DatenToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem_Show = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem_SortUp = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem_SortDown = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem_search = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HilfeToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DataGridView_Adressen = New System.Windows.Forms.DataGridView()
+        Me.MenuStrip1.SuspendLayout()
+        CType(Me.DataGridView_Adressen, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.DatenToolStripMenuItem, Me.HilfeToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(415, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripMenuItem_open, Me.ToolStripMenuItem_Save, Me.UnterToolStripMenuItem_SaveAs, Me.ToolStripMenuItem_exit})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'ToolStripMenuItem_open
+        '
+        Me.ToolStripMenuItem_open.Name = "ToolStripMenuItem_open"
+        Me.ToolStripMenuItem_open.Size = New System.Drawing.Size(156, 22)
+        Me.ToolStripMenuItem_open.Text = "öffnen"
+        '
+        'ToolStripMenuItem_Save
+        '
+        Me.ToolStripMenuItem_Save.Name = "ToolStripMenuItem_Save"
+        Me.ToolStripMenuItem_Save.Size = New System.Drawing.Size(156, 22)
+        Me.ToolStripMenuItem_Save.Text = "speichern"
+        '
+        'UnterToolStripMenuItem_SaveAs
+        '
+        Me.UnterToolStripMenuItem_SaveAs.Name = "UnterToolStripMenuItem_SaveAs"
+        Me.UnterToolStripMenuItem_SaveAs.Size = New System.Drawing.Size(156, 22)
+        Me.UnterToolStripMenuItem_SaveAs.Text = "speichern unter"
+        '
+        'ToolStripMenuItem_exit
+        '
+        Me.ToolStripMenuItem_exit.Name = "ToolStripMenuItem_exit"
+        Me.ToolStripMenuItem_exit.Size = New System.Drawing.Size(156, 22)
+        Me.ToolStripMenuItem_exit.Text = "beenden"
+        '
+        'DatenToolStripMenuItem
+        '
+        Me.DatenToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripMenuItem_Show, Me.ToolStripMenuItem_SortUp, Me.ToolStripMenuItem_SortDown, Me.ToolStripMenuItem_search})
+        Me.DatenToolStripMenuItem.Name = "DatenToolStripMenuItem"
+        Me.DatenToolStripMenuItem.Size = New System.Drawing.Size(50, 20)
+        Me.DatenToolStripMenuItem.Text = "Daten"
+        '
+        'ToolStripMenuItem_Show
+        '
+        Me.ToolStripMenuItem_Show.Name = "ToolStripMenuItem_Show"
+        Me.ToolStripMenuItem_Show.Size = New System.Drawing.Size(181, 22)
+        Me.ToolStripMenuItem_Show.Text = "anzeigen"
+        '
+        'ToolStripMenuItem_SortUp
+        '
+        Me.ToolStripMenuItem_SortUp.Name = "ToolStripMenuItem_SortUp"
+        Me.ToolStripMenuItem_SortUp.Size = New System.Drawing.Size(181, 22)
+        Me.ToolStripMenuItem_SortUp.Text = "aufsteigend sotieren"
+        '
+        'ToolStripMenuItem_SortDown
+        '
+        Me.ToolStripMenuItem_SortDown.Name = "ToolStripMenuItem_SortDown"
+        Me.ToolStripMenuItem_SortDown.Size = New System.Drawing.Size(181, 22)
+        Me.ToolStripMenuItem_SortDown.Text = "absteigend sotieren"
+        '
+        'ToolStripMenuItem_search
+        '
+        Me.ToolStripMenuItem_search.Name = "ToolStripMenuItem_search"
+        Me.ToolStripMenuItem_search.Size = New System.Drawing.Size(181, 22)
+        Me.ToolStripMenuItem_search.Text = "suchen"
+        '
+        'HilfeToolStripMenuItem
+        '
+        Me.HilfeToolStripMenuItem.Name = "HilfeToolStripMenuItem"
+        Me.HilfeToolStripMenuItem.Size = New System.Drawing.Size(44, 20)
+        Me.HilfeToolStripMenuItem.Text = "Hilfe"
+        '
+        'DataGridView_Adressen
+        '
+        Me.DataGridView_Adressen.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView_Adressen.Location = New System.Drawing.Point(12, 27)
+        Me.DataGridView_Adressen.Name = "DataGridView_Adressen"
+        Me.DataGridView_Adressen.Size = New System.Drawing.Size(391, 332)
+        Me.DataGridView_Adressen.TabIndex = 1
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(415, 371)
+        Me.Controls.Add(Me.DataGridView_Adressen)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "MainForm"
+        Me.Text = "Adressverwaltung"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        CType(Me.DataGridView_Adressen, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem_open As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem_Save As ToolStripMenuItem
+    Friend WithEvents UnterToolStripMenuItem_SaveAs As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem_exit As ToolStripMenuItem
+    Friend WithEvents DatenToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem_Show As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem_SortUp As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem_SortDown As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem_search As ToolStripMenuItem
+    Friend WithEvents HilfeToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents DataGridView_Adressen As DataGridView
+End Class
---
+++ Uebung1/Uebung9/MainForm.vb
@@ -0,0 +1,129 @@
+﻿Imports System.IO
+
+Public Class MainForm
+    Private Adresse_List As New List(Of Adressen)
+    Private CurrentFile As String
+    Private Sub ToolStripMenuItem_exit_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem_exit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub ToolStripMenuItem_Show_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem_Show.Click
+        If Adresse_List.Count = 0 Then
+            MsgBox("Keine Daten geladen")
+            Exit Sub
+        End If
+        Dim Viewer_Form As New Viewer(Adresse_List)
+        Viewer_Form.Show()
+    End Sub
+
+    Private Sub ToolStripMenuItem_open_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem_open.Click
+
+        Adresse_List.Clear()
+
+        Dim openFD As New OpenFileDialog()
+
+
+        openFD.InitialDirectory = "C:\Users\Arno\Desktop"
+        openFD.Filter = "Korrdinaten files (*.adress)|*.adress"
+        openFD.RestoreDirectory = True
+
+
+        If openFD.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
+            CurrentFile = openFD.FileName
+            Using sr As StreamReader = New StreamReader(openFD.FileName())
+                Dim line As String
+                line = sr.ReadLine()
+                Do Until line Is Nothing
+                    Dim value_Array() As String = line.Split(" "c)
+
+                    Dim VorName As String = value_Array(0)
+                    Dim NachName As String = value_Array(1)
+                    Dim PLZ As String = value_Array(2)
+                    Dim Stadt As String = value_Array(3)
+                    Dim Strasse As String = value_Array(4)
+                    Dim HausNr As String = value_Array(5)
+
+                    Adresse_List.Add(New Adressen(VorName, NachName, PLZ, Stadt, Strasse, HausNr))
+
+                    line = sr.ReadLine()
+                Loop
+            End Using
+        End If
+
+        upDateDataGrid(Adresse_List)
+
+    End Sub
+
+    Private Sub ToolStripMenuItem_Save_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem_Save.Click
+        If CurrentFile IsNot Nothing Then
+
+            Using sw As StreamWriter = New StreamWriter(CurrentFile)
+
+                For Each adress As Adressen In Adresse_List
+
+                    sw.WriteLine(adress.getExport)
+
+                Next
+                'MsgBox("Export Erfolgreich")
+            End Using
+        Else
+            MsgBox("Export Datei Fehlerhaft")
+        End If
+    End Sub
+
+    Private Sub UnterToolStripMenuItem_SaveAs_Click(sender As Object, e As EventArgs) Handles UnterToolStripMenuItem_SaveAs.Click
+
+        Dim SaveDia As New SaveFileDialog()
+
+        SaveDia.InitialDirectory = "C:\Users\Arno\Desktop"
+        SaveDia.Filter = "Korrdinaten files (*.adress)|*.adress"
+        SaveDia.RestoreDirectory = True
+        SaveDia.AddExtension = True
+
+        If SaveDia.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
+
+
+            Using sw As StreamWriter = New StreamWriter(SaveDia.FileName())
+
+                For Each adress As Adressen In Adresse_List
+
+                    sw.WriteLine(adress.getExport)
+
+                Next
+                'MsgBox("Export Erfolgreich")
+            End Using
+
+        End If
+    End Sub
+
+    Private Sub ToolStripMenuItem_SortUp_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem_SortUp.Click
+        Adresse_List.Sort()
+    End Sub
+
+    Private Sub ToolStripMenuItem_SortDown_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem_SortDown.Click
+        Adresse_List.Sort()
+        Adresse_List.Reverse()
+    End Sub
+
+    Private Sub upDateDataGrid(Adresse_List As List(Of Adressen))
+        DataGridView_Adressen.DataSource = Adresse_List
+    End Sub
+
+
+    Private Sub ToolStripMenuItem_search_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem_search.Click
+        Dim SearchValue As String = InputBox("Geben sie den Suchbegriff ein")
+
+        If Not String.IsNullOrEmpty(SearchValue) Then
+            Dim Ad As Adressen = Adresse_List.Find(Function(x) (x.contains(SearchValue)))
+            If Ad IsNot Nothing Then
+                MsgBox(Ad.Vorname & vbCrLf & Ad.Nachname & vbCrLf & Ad.PLZ & vbCrLf & Ad.Stadt & vbCrLf & Ad.Strasse & vbCrLf & Ad.HausNr)
+            Else
+                MsgBox("Kein Eintrag Gefunden")
+            End If
+
+        End If
+
+    End Sub
+
+
+End Class
