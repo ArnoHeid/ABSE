8df85f8c3f6910354654324e669e0f592a58d9e7
ArnoHeid <arno.dario.ingo.heidelberg@students.hs-mainz.de>
2015-11-08 20:06:54 +0100

    Auf NetMath umgesiegen

---
+++ Uebung1/Project/Helmert2D.vb
@@ -0,0 +1,37 @@
+﻿Imports MathNet.Numerics.LinearAlgebra
+Imports Project
+
+Public Class Helmert2D
+    Implements ITransformation
+
+    Public Property A_Matrix As Matrix(Of Double) Implements ITransformation.A_Matrix
+        Get
+            Throw New NotImplementedException()
+        End Get
+        Set(value As Matrix(Of Double))
+            Throw New NotImplementedException()
+        End Set
+    End Property
+
+    Public Property l_Vektor As Vector(Of Double) Implements ITransformation.l_Vektor
+        Get
+            Throw New NotImplementedException()
+        End Get
+        Set(value As Vector(Of Double))
+            Throw New NotImplementedException()
+        End Set
+    End Property
+
+    Public Property u_Vektor As Vector(Of Double) Implements ITransformation.u_Vektor
+        Get
+            Throw New NotImplementedException()
+        End Get
+        Set(value As Vector(Of Double))
+            Throw New NotImplementedException()
+        End Set
+    End Property
+
+    Public Sub transform() Implements ITransformation.transform
+        Throw New NotImplementedException()
+    End Sub
+End Class
---
+++ Uebung1/Project/ITransformation.vb
@@ -0,0 +1,11 @@
+﻿Imports Project
+Imports MathNet.Numerics.LinearAlgebra
+Public Interface ITransformation
+
+    Property A_Matrix As Matrix(Of Double)
+    Property l_Vektor As Vector(Of Double)
+    Property u_Vektor As Vector(Of Double)
+
+    Sub transform()
+
+End Interface
